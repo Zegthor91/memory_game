76fd6943115db078d2dd91b1e362e299d52abd52
Zegthor91 <zegthorthechazz@gmail.com>
2026-01-01 18:12:23 +0100

    test commit

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <h1>Hello World!</h1>
 </template>
 
 <style scoped></style>
